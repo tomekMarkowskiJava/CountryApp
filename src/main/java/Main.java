e0ea907b5445b6b8957e6a584b1d81aea6346988
Tomasz Markowski <tomekmarkowski92@gmail.com>
2019-10-29 20:05:03 +0100

    Dodano konwertowanie zawartości pliku JSON do listy obiektów "countries" (GSON). Rozbudowano klasę "Country" o pola oraz settery i gettery.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,11 @@
 import Quiz.Quiz;
+import com.google.gson.internal.LinkedTreeMap;
+
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Quiz quiz = new Quiz();
 
     }
 }
--- src/main/java/Quiz/Country.java
+++ src/main/java/Quiz/Country.java
@@ -3,4 +3,36 @@ package Quiz;
 public class Country {
     String name;
     String capital;
+    String region;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCapital() {
+        return capital;
+    }
+
+    public void setCapital(String capital) {
+        this.capital = capital;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    @Override
+    public String toString() {
+        return "Country name:" + name +
+                ", capital city: " + capital +
+                ", region: " + region;
+    }
 }
