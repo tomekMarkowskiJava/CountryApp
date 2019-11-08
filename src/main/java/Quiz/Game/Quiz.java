d63aa3cb84891a924236283f6fbad34b8a7f1469
Tomasz Markowski <tomekmarkowski92@gmail.com>
2019-11-09 00:11:12 +0100

    Dodano odpowiedzi tylko z wybranego regionu oraz system wczytywania wyboru odpowiedzi od użytkownika. Dodano system punktów w skali 0-10.

--- src/main/java/Quiz/Game/Quiz.java
+++ src/main/java/Quiz/Game/Quiz.java
@@ -17,7 +17,7 @@ public class Quiz {
 
     private String jsonText = reading();
     private List<Country> countries = createListOfCountries(jsonText);
-
+    String dots = "**************";
 
     public Quiz() throws IOException {
     }
@@ -42,30 +42,46 @@ public class Quiz {
 
     private void play() throws InterruptedException {
         String region = choseRegion();
-        System.out.println("You have choosen: " + region);
+        System.out.println("You have choosen: " + region + ".\n" + dots + "\n");
         int numberOfQuestions = 10;
+        int points = 0;
 
-        for (int i = 0; i < numberOfQuestions;) {
+        for (int i = 0; i < numberOfQuestions; ) {
             Random random = new Random();
             Country questionCountry = countries.get(random.nextInt(249));
             if (!region.equals(questionCountry.region)) {
                 continue;
             }
-            System.out.println("Question no." + (i+1));
+            System.out.println("Question no." + (i + 1));
             System.out.println("What is the capital of " + questionCountry.name + "?");
-            List <String> answers = new ArrayList<String>();
+            List<String> answers = new ArrayList<String>();
             answers.add(questionCountry.capital);
-            for (int j = 0; j < 3; j++) {
-                answers.add(countries.get(random.nextInt(249)).capital);
+            for (int j = 0; j < 3; ) {
+                Country answer = countries.get(random.nextInt(249));
+                if (!(region.equals(answer.region)) && !(answer.capital == null && !(answer.capital.equals(questionCountry.capital)))) {
+                    continue;
+                } else {
+                    answers.add(answer.capital);
+                    j++;
+                }
             }
             Collections.shuffle(answers);
             for (int j = 0; j < 4; j++) {
-                System.out.println(j+1+". " +answers.get(j));
+                System.out.println(j + 1 + ". " + answers.get(j));
             }
-            System.out.println("");
+            Scanner scanner = new Scanner(System.in);
+            int choice = scanner.nextInt();
+            if (answers.get(choice - 1).equals(questionCountry.capital)) {
+                System.out.println("Good answer!");
+                points++;
+            } else {
+                System.out.println("Wrong answer. The capital of " + questionCountry.name + " is " + questionCountry.capital + ".");
+            }
             Thread.sleep(1500);
+            System.out.println("\n" + dots + "\n");
             i++;
         }
+        System.out.println("Your points : " + points + "/10.");
 
     }
 
@@ -81,7 +97,7 @@ public class Quiz {
             } else {
                 regions.add(region);
             }
-            System.out.println(regions.size() + ". " + regions.get(regions.size()-1));
+            System.out.println(regions.size() + ". " + regions.get(regions.size() - 1));
             i++;
         }
         Scanner scanner = new Scanner(System.in);
@@ -91,7 +107,7 @@ public class Quiz {
     }
 
     private void showMenu() {
-        String dots = "**************";
+
         System.out.println(dots + "\n* CountryApp *\n" + dots);
         System.out.println("1. New Game\n2. Answers\n3. Exit");
     }
